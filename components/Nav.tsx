8738cb569bb58444f890186493659c3560a63fc7
joaobr200 <joaoarantesjob@gmail.com>
2022-07-10 09:51:07 -0300

    hooks

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,24 +3,17 @@ import Image from "next/image";
 import { List } from "phosphor-react";
 import { HTMLAttributes, memo, useEffect, useRef, useState } from "react";
 import Button from "./Button";
+import useEventListener from "../hooks/useEventListener";
 
 const Nav = () => {
   const navRef = useRef<HTMLElement>(null);
   const [navOffSetTop, setnavOffSetTop] = useState(0);
-  useEffect(() => {
-    const pageScroll = () => {
-      setnavOffSetTop(window.scrollY);
-    };
-
-    window.addEventListener("scroll", pageScroll);
-
-    return () => window.removeEventListener("scroll", pageScroll);
-  }, []);
+  useEventListener("scroll", () => setnavOffSetTop(window.scrollY));
   return (
     <nav
       ref={navRef}
       className={`fixed z-[200] top-0 left-0 w-full h-[56px] transition-colors ${
-        navOffSetTop >= 200 && "bg-white"
+        navOffSetTop >= 100 && "bg-white"
       }`}
     >
       <div className="flex items-center justify-between h-full px-4">
@@ -28,7 +21,7 @@ const Nav = () => {
           <Logo />
         </div>
         <div className="flex items-center justify-center gap-2">
-          {navOffSetTop >= 200 ? (
+          {navOffSetTop >= 100 ? (
             <Button href="#" css="w-[142px] h-[28px]">
               <span className="text-[12px] py-1 text-gray-500">
                 ABRA SUA CONTA
@@ -44,9 +37,10 @@ const Nav = () => {
               </span>
             </a>
           )}
-          <div className="cursor-pointer">
+          <button type="button">
+            <span className="sr-only">Abrir menu</span>
             <List size={16} />
-          </div>
+          </button>
         </div>
       </div>
     </nav>
