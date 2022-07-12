c33e7ac692bde150a520d8d1bacb3cf09f6a905f
joaobr200 <joaoarantesjob@gmail.com>
2022-07-12 09:07:02 -0300

    style config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/*.tsx", "./components/*.tsx"],
+  content: ["./pages/*.tsx", "./components/**/*.tsx"],
   theme: {
     extend: {
       colors: {
@@ -29,6 +29,10 @@ module.exports = {
       boxShadow: {
         "3xl":
           "0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%)",
+      },
+      maxWidth: {
+        containerMd: "960px",
+        containerLg: "1280px",
       },
     },
   },
