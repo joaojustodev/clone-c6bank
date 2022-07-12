19e6a5dde9e821c96eb1a364efd3afe6662a7480
joaobr200 <joaoarantesjob@gmail.com>
2022-07-12 09:06:25 -0300

    grid layout

--- components/Home/Products.tsx
+++ components/Home/Products.tsx
@@ -2,22 +2,31 @@ import ProductsForYou from "./ProductsForYou";
 
 const Products = () => {
   return (
-    <section className="py-8 overflow-hidden">
-      <div className="flex flex-col gap-12 px-4 mb-12">
-        <h2 className="text-3xl">
-          Um banco <strong>transparente e completo</strong> com produtos
-          exclusivos
-        </h2>
-        <div className="flex">
-          <h3 className={`px-3 py-[6px] ${true && "opacity-100"}`}>
-            <span>PARA VOCÊ</span>
-          </h3>
-          <h3 className={`px-3 py-[6px] opacity-70`}>
-            <span>PARA MEI</span>
-          </h3>
-          <h3 className={`px-3 py-[6px] opacity-70`}>
-            <span>PARA EMPRESAS</span>
-          </h3>
+    <section className="py-12 md:py-20 md:px-12">
+      <div className="max-w-containerMd mx-auto ">
+        <div className="flex flex-col gap-12 px-4 mb-12">
+          <h2 className="text-3xl leading-snug md:text-[40px] max-w-[633px]">
+            Um banco <strong>transparente e completo</strong> com produtos
+            exclusivos
+          </h2>
+          <div className="flex">
+            <h3 className={`px-3 py-[6px] ${true && "opacity-100"}`}>
+              <span
+                className={`${
+                  true &&
+                  "relative before:absolute before:top-[20px] before:left-[27px]  before:content-[''] before:w-[25%] before:h-[2px] before:bg-yellow-500"
+                }`}
+              >
+                PARA VOCÊ
+              </span>
+            </h3>
+            <h3 className={`px-3 py-[6px] opacity-70`}>
+              <span>PARA MEI</span>
+            </h3>
+            <h3 className={`px-3 py-[6px] opacity-70`}>
+              <span>PARA EMPRESAS</span>
+            </h3>
+          </div>
         </div>
       </div>
 
