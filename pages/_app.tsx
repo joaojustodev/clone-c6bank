9a9ee3fbc4a636127b2df37e0e68f546806b3d6b
joaobr200 <joaoarantesjob@gmail.com>
2022-07-14 23:19:27 -0300

    contexts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { MobileMenuContextProvider } from "../context/MobileMenuContext";
+import { MobileMenuContextProvider } from "../contexts/MobileMenuContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
